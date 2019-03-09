05207fd92172e5cb8df0adb702d6040dc5bb4ebe
Teemu Erkkola <teemu.erkkola@gofore.com>
2019-03-09 14:37:13 +0200

    add ids to pins and throws

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,13 @@ use actix_web::{server, App, fs, Result, HttpRequest, Json};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct BowlingPin {
+    id: u32,
     name: String,
     image: String
 }
 #[derive(Debug, Serialize, Deserialize)]
 struct BowlingThrow {
+    id: u32,
     name: String
 }
 #[derive(Debug, Serialize, Deserialize)]
@@ -30,17 +32,17 @@ impl BowlingGame {
         BowlingGame {
             id: id,
             pins: vec![
-                BowlingPin { name: "foo0".into(), image: "image0".into() },
-                BowlingPin { name: "foo1".into(), image: "image1".into() },
-                BowlingPin { name: "foo2".into(), image: "image2".into() },
-                BowlingPin { name: "foo3".into(), image: "image3".into() },
-                BowlingPin { name: "foo4".into(), image: "image4".into() }
+                BowlingPin { id: 0, name: "foo0".into(), image: "image0".into() },
+                BowlingPin { id: 1, name: "foo1".into(), image: "image1".into() },
+                BowlingPin { id: 2, name: "foo2".into(), image: "image2".into() },
+                BowlingPin { id: 3, name: "foo3".into(), image: "image3".into() },
+                BowlingPin { id: 4, name: "foo4".into(), image: "image4".into() }
             ],
             throws: vec![
-                BowlingThrow { name: "skill0".into() },
-                BowlingThrow { name: "skill1".into() },
-                BowlingThrow { name: "skill2".into() },
-                BowlingThrow { name: "skill3".into() }
+                BowlingThrow { id: 0, name: "skill0".into() },
+                BowlingThrow { id: 1, name: "skill1".into() },
+                BowlingThrow { id: 2, name: "skill2".into() },
+                BowlingThrow { id: 3, name: "skill3".into() }
             ]
         }
     }
